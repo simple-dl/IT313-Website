5174e61ed13447682e8c263036d50f663b969c83
mahyarkhafaji <123523870+mahyarkhafaji@users.noreply.github.com>
2023-03-06 23:23:28 -0500

    Add files via upload

---
+++ greenFunctions.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
