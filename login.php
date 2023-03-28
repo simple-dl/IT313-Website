bb427bed7f7ad6ef81d8fd61285b7c264ebed5c0
Kagen491 <123178541+Kagen491@users.noreply.github.com>
2023-03-27 23:11:56 -0400

    Update login.php

--- login.php
+++ login.php
@@ -2,88 +2,57 @@
 
 session_start();
 
-include "front_connection.php";
+$conn = new mysqli('localhost', 'frontend', 'Frontend1$', 'plants');
 
-if (isset($_POST['email_address']) && isset($_POST['password'])) {
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['uname']) && isset($_POST['psw'])) {
 
     function validate($data)
     {
-
         $data = trim($data);
-
         $data = stripslashes($data);
-
         $data = htmlspecialchars($data);
-
         return $data;
-
     }
 
-    $email_address = validate($_POST['email_address']);
+    $username = validate($_POST['uname']);
+    $password = validate($_POST['psw']);
 
-    $password = validate($_POST['password']);
-
-    if (empty($email_address)) {
-
+    if (empty($username)) {
         header("Location: index.html?error=User Name is required");
-
         exit();
-
     } else if (empty($password)) {
-
         header("Location: index.html?error=Password is required");
-
         exit();
-
     } else {
-
-        $sql = "SELECT * FROM User WHERE email_address='$email_address' AND password='$password'";
-
+        $sql = "SELECT * FROM User WHERE username='$username' AND password='$password'";
         $result = mysqli_query($conn, $sql);
 
         if (mysqli_num_rows($result) === 1) {
-
             $row = mysqli_fetch_assoc($result);
-
-            if ($row['email_address'] === $email_address && $row['password'] === $password) {
-
+            if ($row['username'] === $username && $row['password'] === $password) {
                 echo "Logged in!";
-
+                $_SESSION['username'] = $row['username'];
                 $_SESSION['email_address'] = $row['email_address'];
-
-                $_SESSION['first_name'] = $row['first_name'];
-                
-                $_SESSION['last_name'] = $row['last_name'];
-
+                $_SESSION['password'] = $row['password'];
                 $_SESSION['user_ID'] = $row['user_ID'];
-
                 header("Location: homepage.html");
-
                 exit();
-
             } else {
-
                 header("Location: index.html?error=Incorect User name or password");
-
                 exit();
-
             }
-
         } else {
-
             header("Location: index.html?error=Incorect User name or password");
-
             exit();
-
         }
-
     }
-
 } else {
-
     header("Location: homepage.html");
-
     exit();
-
 }
 
+?>
