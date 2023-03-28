ae90c5094c1c26c52812e5c65c93b2447eff63f0
mahyarkhafaji <123523870+mahyarkhafaji@users.noreply.github.com>
2023-03-27 23:44:45 -0400

    Update greenFunctions.js

--- greenFunctions.js
+++ greenFunctions.js
@@ -1,18 +1,19 @@
-var modal = document.getElementById('id01');
+// Get the modal
+var radishModal = document.getElementById("radishModal");
+
+// When the user clicks on the button, open the modal
+function btnModal(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function modalClose(id) {
+  document.getElementById(id).style.display = "none";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Used to toggle the menu on small screens when clicking on the menu button
-function myFunction() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else { 
-      x.className = x.className.replace(" w3-show", "");
-    }
+  if (event.target == radishModal) {
+    radishModal.style.display = "none";
   }
+}
