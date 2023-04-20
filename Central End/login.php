57099314d2e65ee1c480248d86d6380e74614e5d
Kagen491 <123178541+Kagen491@users.noreply.github.com>
2023-04-20 11:59:45 -0400

    Update login.php

--- Central End/login.php
+++ Central End/login.php
@@ -2,11 +2,7 @@
 
 session_start();
 
-$conn = new mysqli('localhost', 'frontend', 'Frontend1$', 'plants');
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+require 'front_connection.php';
 
 if (isset($_POST['uname']) && isset($_POST['psw'])) {
 
@@ -28,31 +24,31 @@ if (isset($_POST['uname']) && isset($_POST['psw'])) {
         header("Location: index.html?error=Password is required");
         exit();
     } else {
-        $sql = "SELECT * FROM User WHERE username='$username' AND password='$password'";
+        $sql = "SELECT * FROM User WHERE username='$username'";
         $result = mysqli_query($conn, $sql);
 
         if (mysqli_num_rows($result) === 1) {
             $row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $username && $row['password'] === $password) {
+            if (password_verify($password, $row['hashed_pass'])) {
                 echo "Logged in!";
                 $_SESSION['username'] = $row['username'];
                 $_SESSION['email_address'] = $row['email_address'];
-                $_SESSION['password'] = $row['password'];
                 $_SESSION['user_ID'] = $row['user_ID'];
-                header("Location: homepage.html");
+                header("Location: LoggedInHomepage.html");
                 exit();
             } else {
-                header("Location: index.html?error=Incorect User name or password");
+                header("Location: index.html?error=Incorrect password");
                 exit();
             }
         } else {
-            header("Location: index.html?error=Incorect User name or password");
+            header("Location: index.html?error=Incorrect username");
             exit();
         }
     }
 } else {
-    header("Location: homepage.html");
+    header("Location: LoggedInHomepage.html");
     exit();
 }
 
 ?>
+
