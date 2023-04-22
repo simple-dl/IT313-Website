ab323af34de9435b8ec9630c9825462bf3b8af3f
strawberry-frog <124286411+strawberry-frog@users.noreply.github.com>
2023-04-22 14:48:16 -0400

    Create addCabbage.php

---
+++ UseCase1/Front End/addCabbage.php
@@ -0,0 +1,48 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+require 'front_connection.php';
+
+if (isset($_POST['cabbageConfirm'])) {
+    $result = mysqli_fetch_assoc(mysqli_query($conn, "SELECT preset_id_shelf1 FROM User WHERE username='".$_SESSION["username"]."'"));
+    $result2 = mysqli_fetch_assoc(mysqli_query($conn, "SELECT preset_id_shelf2 FROM User WHERE username='".$_SESSION["username"]."'"));
+
+    if ($result['preset_id_shelf1'] == 4) {
+        $sql = "UPDATE User SET preset_id_shelf1='3' WHERE username='".$_SESSION["username"]."'";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "<script type = 'text/javascript'>";
+                echo "top.location.href = 'yourPlant.php';";
+                echo "alert('Plant Added');";
+                 echo "</script>";
+            exit();
+        } else {
+            echo "Error updating database: " . mysqli_error($conn);
+        }
+    } elseif ($result2['preset_id_shelf2'] == 4) {
+        $sql = "UPDATE User SET preset_id_shelf2='3' WHERE username='".$_SESSION["username"]."'";
+
+        if (mysqli_query($conn, $sql)) {
+           echo "<script type = 'text/javascript'>";
+                echo "top.location.href = 'yourPlant.php';";
+                echo "alert('Plant Added');";
+                 echo "</script>";
+            exit();
+        } else {
+            echo "Error updating database: " . mysqli_error($conn);
+        }
+    } else {
+      echo "<script type = 'text/javascript'>";
+       echo "alert('Shelves are Full!');";
+       echo "top.location.href = 'yourPlant.php';";
+       echo "</script>";
+       exit();
+    }
+
+    mysqli_close($conn);
+}
+?>
