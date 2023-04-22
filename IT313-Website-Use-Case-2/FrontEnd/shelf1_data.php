80f1ba622039507a0a327eecb67ea6c30d22a7db
strawberry-frog <124286411+strawberry-frog@users.noreply.github.com>
2023-04-22 15:02:54 -0400

    Update shelf1_data.php

--- IT313-Website-Use-Case-2/FrontEnd/shelf1_data.php
+++ IT313-Website-Use-Case-2/FrontEnd/shelf1_data.php
@@ -1,4 +1,4 @@
-?php
+<?php
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -7,28 +7,28 @@ error_reporting(E_ALL);
 
 $conn = new mysqli('localhost', 'frontend', 'Frontend1$', 'plants');
 
-$sql = "SELECT humidity_percentage FROM self_readingtable WHERE username ='".$_SESSION['username']."'";
+$sql = "SELECT humidity_percentage FROM shelf_reading WHERE username ='".$_SESSION['username']."'";
         $result = mysqli_fetch_assoc(mysqli_query($conn, $sql));
 
-$result = mysqli_query($conn, $sql); 
+$result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
+if (mysqli_num_rows($result) >= 0) {
 
         $row = mysqli_fetch_assoc($result);
-        echo "Humidity Percentage: <span> " . $row["humidity_percentage"]. " % </span> <br>";
+        echo "Humdity Percentage: <span> " . $row["humidity_percentage"]. " % </span> <br>";
 
 } else {
 
        echo "Humdity Percentage: No data Found. <br>";
 }
 
-$sql = "SELECT temperature_F FROM self_readingtable WHERE username ='".$_SESSION['username']."'";
+$sql = "SELECT temperature_F FROM shelf_reading WHERE username ='".$_SESSION['username']."'";
 $result = mysqli_query($conn, $sql);
 
-if (mysqli_num_rows($result) > 0) {
+if (mysqli_num_rows($result) >= 0) {
 
         $row = mysqli_fetch_assoc($result);
-        echo "Temperature: <span>" . $row["temperature_F"]. " degrees </span> <br>";
+        echo "Temperature: <span>" . $row["temperature_F"]. " degress </span> <br>";
 
 } else {
 
@@ -36,27 +36,21 @@ if (mysqli_num_rows($result) > 0) {
 }
 
 
-$sql = "SELECT lights_off_on FROM self_readingtable WHERE username ='".$_SESSION['username']."'";
+$sql = "SELECT lights_off_on FROM shelf_reading WHERE username ='".$_SESSION['username']."'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) == 1) {
 
         $row = mysqli_fetch_assoc($result);
-        echo "Lights: ON"; 
+        echo "Lights: ON";
 } else if (mysqli_num_rows($result) == 0){
         $row = mysqli_fetch_assoc($result);
-        echo "Lights: OFF"; 
+        echo "Lights: OFF";
 
 } else {
 
        echo "Lights: No data Found. <br>";
-
 }
-
-
-
-mysqli_close($conn);
-
 
 
 ?>
