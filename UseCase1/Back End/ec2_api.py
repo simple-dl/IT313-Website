fc70c3a14be7b0f65490a418e6dc7aa0db2ca798
Phil Isaacs <32342350+LemonCow@users.noreply.github.com>
2023-04-25 11:31:19 -0400

    Update ec2_api.py

--- UseCase1/Back End/ec2_api.py
+++ UseCase1/Back End/ec2_api.py
@@ -37,5 +37,30 @@ def get_data():
         print("Failed to send data to API")
         return jsonify({"message": "Failed"})
 
+@app.route('/data', methods=['POST'])
+def get_data():
+
+    data = request.get_json()
+
+    temperature = data['temperature']
+    humidity = data['humidity']
+
+    cursor = db.cursor()
+
+    sql = "UPDATE shelf_reading SET humidity_percentage = %s, temperature_C = %s WHERE username = 'test1'"
+    val = (humidity, temperature)
+    cursor.execute(sql, val)
+
+    db.commit()
+    cursor.close()
+
+    print('Received POST request for data')
+
+    print(temperature)
+    print(humidity)
+
+    return jsonify({'message': 'Data received successfully'})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
