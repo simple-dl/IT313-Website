360c00c371eb41765edac16d84a8d357a219492e
strawberry-frog <124286411+strawberry-frog@users.noreply.github.com>
2023-04-25 19:36:31 -0400

    Update shelf1_data.php

--- UseCase2/Front End/shelf1_data.php
+++ UseCase2/Front End/shelf1_data.php
@@ -6,62 +6,42 @@ error_reporting(E_ALL);
 
 
 $conn = new mysqli('localhost', 'frontend', 'Frontend1$', 'plants');
-
 $sql = "SELECT humidity_percentage FROM shelf_reading WHERE username ='".$_SESSION['username']."'";
         $result = mysqli_fetch_assoc(mysqli_query($conn, $sql));
-
 $result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
 
-if (mysqli_num_rows($result) > 0) {
+if (is_null($row["humidity_percentage"])) {
 
-        $row = mysqli_fetch_assoc($result);
-        echo "Humidity Percentage: <span> " . $row["humidity_percentage"]. " % </span> <br>";
-
+        echo "Humidity: <span style='color: red;'> No data found. <br></span>";
 } else {
 
-       echo "Humdity Percentage: No data Found. <br>";
+
+       echo "Humidity: <span> " . $row["humidity_percentage"]."% </span> <br>";
 }
 
 $sql = "SELECT temperature_C FROM shelf_reading WHERE username ='".$_SESSION['username']."'";
 $result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result); // fetch result once and store in variable
 
-if (mysqli_num_rows($result) > 0) {
-
-        $row = mysqli_fetch_assoc($result);
-        echo "Temperature: <span>" . $row["temperature_F"]. " &#176 </span> <br>";
-
+if (is_null($row["temperature_C"])) {
+    echo "Temperature: <span style='color: red;'> No data found. <br></span>";
 } else {
-
-       echo "Temperature: No data Found. <br>";
+    echo "Temperature: <span>" . $row["temperature_C"]."&#176C</span> <br>";
 }
-
-
-//$sql = "SELECT temperature_F FROM shelf_reading WHERE username ='".$_SESSION['username']."'"; MOISTURE
-//$result = mysqli_query($conn, $sql);
-
-//if (mysqli_num_rows($result) > 0) {
-
-  //      $row = mysqli_fetch_assoc($result);
- //       echo "Moisture: <span>" . $row[""]. "</span><br>";
-
-//} else {
-//
- //      echo "Moisture: No data found. <br>";
-//}
 
 $sql = "SELECT lights_off_on FROM shelf_reading WHERE username ='".$_SESSION['username']."'";
 $result = mysqli_query($conn, $sql);
-
-if (mysqli_fetch_assoc($result) == 1) {
-        echo "Lights: ON";
-} else if (mysqli_fetch_assoc($result) == 0){
-
-        echo "Lights: OFF";
-
-} else if (mysqli_fetch_assoc($result) != 1 && mysqli_fetch_assoc($result) != 0){
-
-       echo "Lights: No data Found. <br>";
+$row = mysqli_fetch_assoc($result);
+if(is_null($row["lights_off_on"])) {
+        echo "Lights:<span style='color: red;'> No data found. <br></span>";
+}
+else if (mysqli_fetch_assoc($result) == 1) {
+        echo "Lights: ON <br>";
+} else if (mysqli_fet  echo "Lights: OFF<br>";
 
 }
 
 ?>
+                                                                                                                                                                                         47,2          Botch_assoc($result) == 0){
+        
