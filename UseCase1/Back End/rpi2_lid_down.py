f144e047ccd6c272c54a927e0ad35582c9e6134e
Phil Isaacs <32342350+LemonCow@users.noreply.github.com>
2023-05-05 20:41:16 -0400

    Create rpi2_lid_down.py

---
+++ UseCase1/Back End/rpi2_lid_down.py
@@ -0,0 +1,12 @@
+from time import *
+from adafruit_servokit import ServoKit
+
+kit = ServoKit(channnels=16)
+
+kit.continuous_servo[0].throttle = 1
+
+print("Closing the lid")
+sleep(5.5)
+print("The lid is close")
+
+kit.continuous_servo[0].throttle = 0
